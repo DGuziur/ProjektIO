d9aaf48973c308e8aeb0db362a27a6c6e18bd00c
Falalatina <92568943+Falalatina@users.noreply.github.com>
2022-02-09 17:54:41 +0100

    Add files via upload

---
+++ bakainator.py
@@ -0,0 +1,11 @@
+#program sluzacy bakowaniu pana d
+a= input("Podaj wartosc baki")
+b = input("A kto baka D czy K")
+
+
+if str(b) =="D" :
+    print("Bakusssssssss")
+    if int(a) >= 3:
+        print("Bakaaaaaa " * int(a))
+else:
+    print("a to nie ac pac :> u re greatest!!!!")
