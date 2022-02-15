7c0b8e1ef8fcbc2b055f4252099a11aa27f11b08
Falalatina <karolina40.gorska17@gmail.com>
2022-02-15 16:16:50 +0100

    Testy jedn

--- bakainator.py
+++ bakainator.py
@@ -10,3 +10,23 @@ if str(b) =="D" :
 else:
     print("a to nie ac pac :> u re greatest!!!!")
 #test uwu
+
+#sprawdzamy czy baki enough
+
+def is_baka(en: int) -> bool:
+    pass
+
+def test_is_baka():
+    #given
+    en= 3
+    #when
+    result = is_baka(en)
+    #then
+    assert result
+    assert is_baka(0)
+
+#sprawdzam, czy nie baka
+
+def test_is_not_baka():
+    assert not is_baka(1)
+    assert not is_baka(2)
