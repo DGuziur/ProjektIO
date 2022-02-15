318ef286384e965c46a31a09906707590149f616
DGuziur <43893166+DGuziur@users.noreply.github.com>
2022-02-15 17:14:56 +0100

    unit test

--- IO/testy.js
+++ IO/testy.js
@@ -1,4 +1,5 @@
 function cartTest(){
-    console.assert( cart != [], "Cart error - is not an empty array")
-    console.assert(totalPrice != 0, "error")
+    console.assert(cart != [], "Cart error - is not an empty array")
+    console.assert(totalPrice !== 0, "error with total price (not starting at == 0)")
+    console.assert(totalItems !== 0, "error with total items (not starting at == 0)")
 }
